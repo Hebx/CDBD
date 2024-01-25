5e2dc23ed6eecec8878e1e541aa0a084fd145353
Hebx <lord.noctx@gmail.com>
2024-01-25 10:56:55 +0100

    Added deploy test script

--- tests/cdbd_spl.ts
+++ tests/cdbd_spl.ts
@@ -1,42 +1,140 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CdbdSpl } from "../target/types/cdbd_spl";
+import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, LAMPORTS_PER_SOL,ParsedAccountData } from "@solana/web3.js";
+import { Metaplex } from "@metaplex-foundation/js";
+import { assert } from "chai";
+
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
+  TOKEN_PROGRAM_ID,
+  getAccount
+} from "@solana/spl-token";
 
 describe("cdbd_spl", () => {
-  // Configure the client to use the local cluster.
+  // Configure the client to use the devnet cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
+  // Generate a new keypair for the data account for the program
   const dataAccount = anchor.web3.Keypair.generate();
-  const wallet = provider.wallet;
+  // Generate a mint keypair
+  const mintKeypair = anchor.web3.Keypair.generate();
+  const wallet = provider.wallet as anchor.Wallet;
+  const connection = provider.connection;
 
   const program = anchor.workspace.CdbdSpl as Program<CdbdSpl>;
 
+  // Metadata for the Token
+  const tokenTitle = "CDBD Token";
+  const tokenSymbol = "CDBD";
+  const tokenUri =
+    "https://raw.githubusercontent.com/hebx/cdbd/assets/cdbd.json";
+
   it("Is initialized!", async () => {
-    // Add your test here.
+    // Initialize data account for the program, which is required by Solang
     const tx = await program.methods
       .new()
       .accounts({ dataAccount: dataAccount.publicKey })
       .signers([dataAccount])
       .rpc();
     console.log("Your transaction signature", tx);
+  });
 
-    const val1 = await program.methods
-      .get()
+
+
+  it("Create cdbd SPL Token!", async () => {
+    // Get the metadata address for the mint
+    const metaplex = Metaplex.make(connection);
+    const metadataAddress = await metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: mintKeypair.publicKey });
+
+    // Create the token mint
+    const tx = await program.methods
+      .createTokenMint(
+        wallet.publicKey, // payer
+        mintKeypair.publicKey, // mint
+        wallet.publicKey, // mint authority
+        wallet.publicKey, // freeze authority
+        metadataAddress, // metadata address
+        9, // decimals
+        tokenTitle, // token name
+        tokenSymbol, // token symbol
+        tokenUri // token uri
+      )
       .accounts({ dataAccount: dataAccount.publicKey })
-      .view();
+      .remainingAccounts([
+        {
+          pubkey: wallet.publicKey,
+          isWritable: true,
+          isSigner: true,
+        },
+        { pubkey: mintKeypair.publicKey, isWritable: true, isSigner: true },
+        {
+          pubkey: new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"), // Metadata program id
+          isWritable: false,
+          isSigner: false,
+        },
+        { pubkey: metadataAddress, isWritable: true, isSigner: false },
+        { pubkey: SystemProgram.programId, isWritable: false, isSigner: false },
+        { pubkey: SYSVAR_RENT_PUBKEY, isWritable: false, isSigner: false },
+      ])
+      .signers([mintKeypair])
+      .rpc({ skipPreflight: true });
+    console.log("Your transaction signature", tx);
+  });
 
-    console.log("state", val1);
+  it("Mint cdbd tokens to your wallet!", async () => {
+    // Wallet's associated token account address for mint
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      wallet.payer, // payer
+      mintKeypair.publicKey, // mint
+      wallet.publicKey // owner
+    );
 
-    await program.methods
-      .flip()
+    const tx = await program.methods
+      .mintTo(
+        wallet.publicKey, // payer
+        tokenAccount.address, // associated token account address
+        mintKeypair.publicKey, // mint
+        new anchor.BN(199000000000) // amount to mint
+      )
       .accounts({ dataAccount: dataAccount.publicKey })
-      .rpc();
+      .remainingAccounts([
+        {
+          pubkey: wallet.publicKey,
+          isWritable: true,
+          isSigner: true,
+        },
+        { pubkey: tokenAccount.address, isWritable: true, isSigner: false },
+        { pubkey: mintKeypair.publicKey, isWritable: true, isSigner: false },
+        {
+          pubkey: SystemProgram.programId,
+          isWritable: false,
+          isSigner: false,
+        },
+        { pubkey: TOKEN_PROGRAM_ID, isWritable: false, isSigner: false },
+        {
+          pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+          isWritable: false,
+          isSigner: false,
+        },
+      ])
+      .rpc({ skipPreflight: true });
+    console.log("Your transaction signature", tx);
 
-    const val2 = await program.methods
-      .get()
-      .accounts({ dataAccount: dataAccount.publicKey })
-      .view();
+    // Get the minted token amount on the associated token account
+    const tokenAmount = (await getAccount(connection, tokenAccount.address)).amount;
+    console.log("tokenAmount", tokenAmount);
+    // Converting tokenAmount to a regular number using Number()
+    let tokens = Number(tokenAmount);
+    console.log("minted token amounts", tokens / LAMPORTS_PER_SOL);
+    assert.equal(tokens / LAMPORTS_PER_SOL, 199);
+  });
 
-    console.log("state", val2);  });
 });
+
